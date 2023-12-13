19813084b49cf1500bce3642e6b3f288c29135fb
harvey ian <112628848+hidimalibot@users.noreply.github.com>
2023-12-13 15:14:51 +0800

    dep
    22

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
-  } catch (error) {
-    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
-  }
-};
+mongoose.connect(process.env.MONGO_URL);
 
-module.exports = connectDB;
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("MongoDB connection is successful");
+});
+
+connection.on("error", (error) => {
+  console.log("Error in MongoDB connection", error);
+});
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const connectDB = require("./config/dbConfig");
+const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 const path = require("path");
-
-// MongoDB connection
-connectDB();
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
@@ -22,7 +19,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
-
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
